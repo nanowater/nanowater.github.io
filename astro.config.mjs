2ef9025fa7bcb4f6ba52c1e9fa2384a44c8b5e06
nanowater <nanowater718@gmail.com>
2026-07-22 17:33:12 +0900

    RSS 버튼, 목차 추가. 등등...

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,39 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
+import { visit } from 'unist-util-visit';
+
+/** @type {import('unified').Plugin} */
+function rehypeImageCaption() {
+	return (tree) => {
+		visit(tree, 'element', (node, index, parent) => {
+			const el = /** @type {any} */ (node);
+			const par = /** @type {any} */ (parent);
+			if (
+				el.tagName === 'img' &&
+				el.properties?.alt &&
+				par?.tagName === 'p'
+			) {
+				const alt = el.properties.alt;
+				const figure = {
+					type: 'element',
+					tagName: 'figure',
+					properties: {},
+					children: [
+						el,
+						{
+							type: 'element',
+							tagName: 'figcaption',
+							properties: {},
+							children: [{ type: 'text', value: alt }],
+						},
+					],
+				};
+				par.children.splice(index, 1, figure);
+			}
+		});
+	};
+}
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,6 +45,7 @@ export default defineConfig({
 		shikiConfig: {
 			theme: 'one-dark-pro',
 		},
+		rehypePlugins: [/** @type {any} */ (rehypeImageCaption)],
 	},
 	fonts: [
 		{
